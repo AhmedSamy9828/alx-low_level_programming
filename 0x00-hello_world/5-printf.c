a3166374300452331e7c2a4e60de188bb9a415bb
AhmedSamy9828 <Ahmed28Samy98@gmail.com>
2023-12-14 17:59:28 -0800

    5. Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main -  0x00. C - Hello, World
+ * Description : 5. Hello, printf
+ * Return: 0
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
