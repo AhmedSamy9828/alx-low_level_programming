6fe95c07f756c512460826a0112d81ea32af0857
AhmedSamy9828 <Ahmed28Samy98@gmail.com>
2023-12-15 14:41:56 -0800

    5. Numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main -  0x01. C - Variables, if, else, while
+ * Description : 5. Numbers
+ * Return: 0
+*/
+int main(void)
+{
+	int digit = 0;
+
+	while (digit <= 9)
+	{
+		printf("%i", digit);
+		digit++;
+	}
+	putchar('\n');
+
+	return (0);
+}
